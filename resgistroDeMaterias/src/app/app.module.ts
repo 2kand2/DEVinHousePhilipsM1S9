ab3688cb6c04ce6a0d57916755c2343eb8987c7d
Henrique <octaciano11@gmail.com>
2022-12-08 21:28:19 -0300

    Inlcude modal component

--- resgistroDeMaterias/src/app/app.module.ts
+++ resgistroDeMaterias/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { FormularioRegistroComponent } from './components/formulario-registro/formulario-registro.component';
+import { FormularioRegistroComponent } from './pages/formulario-registro/formulario-registro.component';
+import { ModalComponent } from './components/modal/modal.component';
 
 @NgModule({
-  declarations: [AppComponent, FormularioRegistroComponent],
+  declarations: [AppComponent, FormularioRegistroComponent, ModalComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
--- resgistroDeMaterias/src/app/pages/formulario-registro/formulario-registro.component.ts
+++ resgistroDeMaterias/src/app/pages/formulario-registro/formulario-registro.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-formulario-registro',
   templateUrl: './formulario-registro.component.html',
-  styleUrls: ['./formulario-registro.component.scss']
+  styleUrls: ['./formulario-registro.component.scss'],
 })
 export class FormularioRegistroComponent {
+  constructor() {}
 
+  titleModal = 'Formulário enviado!';
+  messageModal = 'O formulário foi enviado com sucesso!';
 }
