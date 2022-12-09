ab9d404e4c82f45e0f742903fd4742a061416d70
Henrique <octaciano11@gmail.com>
2022-12-08 23:04:20 -0300

    include navbar component

--- resgistroDeMaterias/src/app/app.module.ts
+++ resgistroDeMaterias/src/app/app.module.ts
@@ -4,9 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FormularioRegistroComponent } from './pages/formulario-registro/formulario-registro.component';
 import { ModalComponent } from './components/modal/modal.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent, FormularioRegistroComponent, ModalComponent],
+  declarations: [
+    AppComponent,
+    FormularioRegistroComponent,
+    ModalComponent,
+    NavbarComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
