a9d7058e330eb66fa3efb6561b1cc2dd61bb9a57
Henrique <octaciano11@gmail.com>
2022-12-10 04:31:04 -0300

    Alteração de acesso aos dados pelo componente item-card

--- resgistroDeMaterias/src/app/components/item-card/item-card.component.ts
+++ resgistroDeMaterias/src/app/components/item-card/item-card.component.ts
@@ -6,8 +6,11 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./item-card.component.scss'],
 })
 export class ItemCardComponent {
-  @Input() srcImg = '';
-  @Input() name = '';
-  @Input() nameActor = '';
-  @Input() descriptionCard = '';
+  personagem: any = {
+    nome: 'Charlie Francis Harper',
+    imagem: '../../../assets/img/two-and-a-half-men-charlie-sheen-.jpg',
+    nomeAtor: 'Charlie Sheen',
+    descricao:
+      'Charles Francis Harper é um solteiro de meia-idade que mora em uma casa de praia em Malibu. Seu estilo de vida frívolo se deve ao fato de ganhar muito dinheiro escrevendo jingles no início e depois canções infantis. Ele bebe e dorme com mulheres diferentes quase todos os dias, e então acorda na manhã seguinte pronto para fazer tudo de novo.',
+  };
 }
